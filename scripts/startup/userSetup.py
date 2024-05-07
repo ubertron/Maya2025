3b78d18a779ede0eed99c8c15f854ac886719c58
ubertron <andy_j_davis@yahoo.com>
2024-05-07 16:52:42 -0700

    userSetup.py: Small update to comment

--- scripts/startup/userSetup.py
+++ scripts/startup/userSetup.py
@@ -6,7 +6,7 @@ from maya import cmds
 
 
 def main():
-    cmds.scriptEditorInfo(clearHistory=True)    # TEMP! Makes my life easier while I'm setting up the environment
+    cmds.scriptEditorInfo(clearHistory=True)    # Debug only: comment out to view full startup log
     script_path = Path.home().joinpath('Dropbox/Technology/Python3/Projects/Maya2025/scripts/startup')
     logging.info(f">>> Maya2025 Project userSetup.py in {script_path}")
 
